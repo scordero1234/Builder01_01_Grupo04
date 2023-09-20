fa3a06946e5baee34d90c1d95c8c4d76133a31ab
e.aguilar <e.aguilar@jardinazuayo.fin.ec>
2023-09-20 09:39:35 -0500

    proyecto inicial

---
+++ Builder01_01_Grupo04/src/main/java/ups/edu/ec/view/Builder01_01_Grupo04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ups.edu.ec.view;
+
+/**
+ *
+ * @author e.aguilar
+ */
+public class Builder01_01_Grupo04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
